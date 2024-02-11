1591d7c8b33115fca18efd0276dd327916ceafe6
Daniel Yachnikov Hughes <daniel.lloyd.hughes@gmail.com>
2024-02-11 22:54:04 +0000

    made graph chart change to colour function

--- components/RowCoinItem/index.tsx
+++ components/RowCoinItem/index.tsx
@@ -21,8 +21,6 @@ const RowCoinItem = ({ coin }: { coin: Coin }) => {
 
   // const graphLine: number[] = coin.sparkline_in_7d.price;
 
-  const graphLine7d: number[] = coin.sparkline_in_7d.price;
-
   return (
     <div className=" bg-black w-full  text-[#DEDEDE] text-sm font-light p-5 flex gap-3 items-center">
       <span>{coin.market_cap_rank}</span>
@@ -79,7 +77,7 @@ const RowCoinItem = ({ coin }: { coin: Coin }) => {
       </span>
       <span className="w-[14%] pl-3 h-[50px]">
         <PriceCoinGraph
-          prices={graphLine7d}
+          prices={coin.sparkline_in_7d.price}
           priceChange={priceChange7d}
           reduceBy={6}
         />
