0aaae0e560aa4343f70948ead62a4ef6fe59b6e8
Daniel Yachnikov Hughes <daniel.lloyd.hughes@gmail.com>
2024-02-29 12:28:50 +0000

    fixed search nav bug

--- components/SearchBar/index.tsx
+++ components/SearchBar/index.tsx
@@ -24,7 +24,9 @@ const SearchBar = () => {
       <input
         onChange={handleInputChange}
         type="text"
-        className="w-full bg-gradient-to-r from-black to-gray-900 p-2 rounded-xl outline-none placeholder-white text-xs pl-7 text-white font-light"
+        className={`w-full bg-gradient-to-r from-black to-gray-900 p-2 rounded-xl outline-none placeholder-white text-xs pl-7 text-white font-light ${
+          showDropDown ? "rounded-bl-none rounded-br-none" : ""
+        }`}
         placeholder="Search..."
       />
       {showDropDown && hasCoins && (
