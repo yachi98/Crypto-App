514271015f1de896220b892da08f0087d4708b1e
Daniel Yachnikov Hughes <daniel.lloyd.hughes@gmail.com>
2024-03-03 21:34:35 +0000

    fetches coin data from coin market api

--- components/CoinGraphChart/index.tsx
+++ components/CoinGraphChart/index.tsx
@@ -10,9 +10,11 @@ import {
   LineElement,
   Filler,
   BarElement,
+  ScriptableContext,
 } from "chart.js";
 import { Line, Bar } from "react-chartjs-2";
 import getNumArray from "@/utils/getGraphArray";
+import formatNumber from "@/utils/formatNumber";
 
 ChartJS.register(
   CategoryScale,
@@ -23,7 +25,7 @@ ChartJS.register(
   BarElement
 );
 
-const commonOptions = {
+const options = {
   responsive: true,
   scales: {
     x: {
@@ -56,22 +58,41 @@ const commonOptions = {
   borderWidth: 0,
 };
 
+const getBackgroundColor = (
+  context: ScriptableContext<"line">
+): CanvasGradient => {
+  const ctx: CanvasRenderingContext2D = context.chart.ctx;
+  const height: number = ctx.canvas.clientHeight;
+  const gradientFill: CanvasGradient = ctx.createLinearGradient(
+    0,
+    0,
+    0,
+    height
+  );
+
+  gradientFill.addColorStop(0, "rgba(0, 147, 176, 0.5)");
+  gradientFill.addColorStop(0.7, "rgba(0, 147, 176, 0.1)");
+  gradientFill.addColorStop(1, "transparent");
+  return gradientFill;
+};
+
 const CoinLineGraph = ({ coin }: { coin: Coin }) => {
   const data = {
     labels: getNumArray(coin.prices.length),
     datasets: [
       {
         data: coin.prices,
-        borderColor: "white",
-        borderWidth: 1,
+        borderColor: "rgba(0, 147, 176, 0.5)",
+        borderWidth: 2,
         pointRadius: 0,
         fill: true,
         tension: 0.8,
+        backgroundColor: getBackgroundColor,
       },
     ],
   };
 
-  return <Line options={commonOptions} data={data} />;
+  return <Line options={options} data={data} />;
 };
 
 const CoinBarGraph = ({ coin }: { coin: Coin }) => {
@@ -80,30 +101,56 @@ const CoinBarGraph = ({ coin }: { coin: Coin }) => {
     datasets: [
       {
         data: coin.prices,
-        borderColor: "white",
+        borderColor: "rgba(0, 147, 176, 0.5)",
         borderWidth: 1,
         pointRadius: 0,
         fill: true,
         tension: 0.8,
+        backgroundColor: getBackgroundColor,
       },
     ],
   };
 
-  return <Bar options={commonOptions} data={data} />;
+  return <Bar options={options} data={data} />;
 };
 
 const CoinGraphChart = () => {
   const { selectedCoins } = useAppSelector((state) => state.selectedCoinData);
   const selectedCoin = selectedCoins.length > 0 ? selectedCoins[0] : null;
 
+  const { coinMarketData } = useAppSelector((state) => state.coinMarketData);
+  const coinInfo = coinMarketData.find(
+    (data) => selectedCoin && data.id === selectedCoin.id
+  );
+
+  const todayDate: string = new Date().toLocaleDateString("en-US", {
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+  });
+
   return (
     <div className="flex gap-3 mt-4">
       <div className="w-[50%] h-[450px] bg-black rounded-2xl p-2">
-        <h2 className="text-[#DEDEDE] text-xl p-6 mt-3">Volume 24h</h2>
+        {coinInfo && (
+          <div className="flex flex-col">
+            <span className="px-1 text-[#DEDEDE] flex text-sm p-2">
+              {coinInfo.name.charAt(0).toUpperCase() +
+                coinInfo.name.slice(1).toLowerCase()}{" "}
+              ({coinInfo.symbol.toUpperCase()})
+            </span>
+            <span className="w-[6%] px-1 text-[#DEDEDE] text-2xl">
+              {formatNumber(coinInfo.current_price)}
+            </span>
+          </div>
+        )}
         {selectedCoin && <CoinLineGraph coin={selectedCoin} />}
       </div>
       <div className="w-[50%] h-[450px] bg-black rounded-2xl p-2">
-        <h2 className="text-[#DEDEDE] text-xl p-6 mt-3">Volume 24h</h2>
+        <div className="flex flex-col">
+          <span className="text-[#DEDEDE] text-sm p-2">Volume 24h</span>
+          <span className="text-[#DEDEDE] text-sm p-2">{todayDate}</span>
+        </div>
         {selectedCoin && <CoinBarGraph coin={selectedCoin} />}
       </div>
     </div>
