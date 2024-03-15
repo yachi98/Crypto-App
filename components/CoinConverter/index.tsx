6cfab4ef9148ab0098032d84cf95c812f8727f2f
Daniel Yachnikov Hughes <daniel.lloyd.hughes@gmail.com>
2024-03-15 23:49:27 +0000

    finished styling for converter button

--- components/CoinConverter/index.tsx
+++ components/CoinConverter/index.tsx
@@ -1,6 +1,5 @@
 import { useState } from "react";
 import { motion } from "framer-motion";
-import CoinIcon from "@/public/CoinIcon.svg";
 import GraphIcon from "@/public/GraphIcon.svg";
 
 const CoinConverter = () => {
@@ -11,18 +10,7 @@ const CoinConverter = () => {
   };
 
   return (
-    <div className="w-[180px] items-center backdrop-filter bg-gradient-to-r from-black to-gray-900 rounded-xl text-white text-xs font-light flex justify-center">
-      <motion.button
-        className={`${
-          isClicked ? "" : "bg-gradient-to-r from-purple-400 to-orange-300"
-        } p-2 rounded-xl w-[100px] flex gap-1`}
-        onClick={handleToggle}
-        whileTap={{ scale: 1.3 }}
-        transition={{ type: "spring", stiffness: 100, damping: 10 }}
-      >
-        <CoinIcon />
-        Coins
-      </motion.button>
+    <div className="w-[100px] items-center backdrop-filter bg-gradient-to-r from-black to-gray-900 rounded-xl text-white text-xs font-light flex justify-center">
       <motion.button
         className={`${
           isClicked ? "bg-gradient-to-r from-purple-400 to-orange-300" : ""
