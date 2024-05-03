b3b3e454abe1e0d30fb15ced8f190c7c227b56ee
Daniel Yachnikov Hughes <daniel.lloyd.hughes@gmail.com>
2024-05-03 22:26:10 +0100

    created portfolio modal

--- app/portfolio/page.tsx
+++ app/portfolio/page.tsx
@@ -1,9 +1,20 @@
+"use client";
+import { useState } from "react";
+import PortfolioModal from "@/components/PortfolioModal";
+
 const PortfolioPage = () => {
+  const [showModal, setShowModal] = useState(false);
+  const handleClick = () => {
+    setShowModal(!showModal);
+  };
   return (
     <div className="dark:bg-gray-950 bg-light-theme max-w-screen-2xl m-auto h-screen p-2">
       <div className="flex justify-between items-center">
         <h2 className="text-3xl mt-5">Portfolio</h2>
-        <button className="p-5 dark:bg-gray-900 bg-white rounded-2xl text-md mt-5 w-[200px]">
+        <button
+          onClick={() => handleClick()}
+          className="p-5 dark:bg-gray-900 bg-white rounded-2xl text-md mt-5 w-[200px]"
+        >
           Add Asset
         </button>
       </div>
@@ -14,8 +25,8 @@ const PortfolioPage = () => {
         effortlessly. You can add your coins by simply clicking on the Add Asset
         button above.
       </p>
+      {showModal && <PortfolioModal />}
     </div>
   );
 };
-
 export default PortfolioPage;
---
+++ components/PortfolioModal/index.tsx
@@ -0,0 +1,6 @@
+const PortfolioModal = () => {
+  return (
+    <div className="w-[800px] h-[500px] dark:bg-gray-900 bg-white absolute top-1/2 -translate-y-1/2 left-1/2 -translate-x-1/2 rounded-3xl"></div>
+  );
+};
+export default PortfolioModal;
