f8b090de9d60be7383dd0f09b9bd4aa6106c48d3
Daniel Yachnikov Hughes <daniel.lloyd.hughes@gmail.com>
2024-05-10 20:33:13 +0100

    fixed redux implementation

--- redux/features/portfolioSlice.tsx
+++ redux/features/portfolioSlice.tsx
@@ -1,7 +1,6 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "axios";
 import { Portfolio } from "@/interfaces/portfolio.interface";
-import { Coin } from "@/interfaces/coin.interface";
 
 interface PortfolioState {
   portfolioData: Portfolio[];
@@ -22,15 +21,8 @@ export const getPortfolioData = createAsyncThunk(
   async (coinId: string, { rejectWithValue }) => {
     try {
       const { data } = await axios.get(
-        `https://pro-api.coingecko.com/api/v3/coins/${coinId}/history`
+        `https://api.coingecko.com/api/v3/coins/${coinId}/history?date=30-12-2023&localization=false`
       );
-      //   const results = await axios.get(
-      //     `https://api.coingecko.com/api/v3/coins/${coinId}/history`
-      //   );
-      //   const historicalData = results.data;
-      //   return historicalData;
-
-      console.log(data);
       return data;
     } catch (error) {
       return rejectWithValue(error);
@@ -41,29 +33,24 @@ export const getPortfolioData = createAsyncThunk(
 const portfolioSlice = createSlice({
   name: "portfolio",
   initialState,
-  reducers: {
-    addPortfolio(state, action) {
-      state.portfolioData = [...state.portfolioData, action.payload];
-    },
+  reducers: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(getPortfolioData.pending, (state) => {
+        state.isLoading = true;
+        state.hasError = false;
+      })
+      .addCase(getPortfolioData.fulfilled, (state, action) => {
+        state.portfolioData = [...state.portfolioData, action.payload];
+        console.log(action.payload);
+        state.isLoading = false;
+      })
+      .addCase(getPortfolioData.rejected, (state, action) => {
+        state.isLoading = false;
+        state.hasError = true;
+        console.error("API call failed with error:", action.payload);
+      });
   },
-  // extraReducers: (builder) => {
-  //   builder
-  //     .addCase(getPortfolioData.pending, (state) => {
-  //       state.isLoading = true;
-  //       state.hasError = false;
-  //     })
-  //     .addCase(getPortfolioData.fulfilled, (state, action) => {
-  //       state.portfolio = [...state.portfolio, ...action.payload];
-  //       console.log(action.payload);
-  //       state.isLoading = false;
-  //     })
-  //     .addCase(getPortfolioData.rejected, (state, action) => {
-  //       state.isLoading = false;
-  //       state.hasError = true;
-  //       console.error("API call failed with error:", action.payload);
-  //     });
-  // },
 });
 
-export const { addPortfolio } = portfolioSlice.actions;
 export default portfolioSlice.reducer;
