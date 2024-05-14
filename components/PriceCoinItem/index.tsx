4eefa0644ceec2e981f7a174717dc031c74b8373
Daniel Yachnikov Hughes <daniel.lloyd.hughes@gmail.com>
2024-05-15 00:02:45 +0100

    fixed all styling

--- components/PriceCoinItem/index.tsx
+++ components/PriceCoinItem/index.tsx
@@ -37,8 +37,8 @@ const PriceCoinItem = ({ coin }: { coin: Coin }) => {
       onClick={() => coinSelector(coin)}
       className={`rounded-2xl pl-2 border-white  bg-white ${
         isSelected
-          ? "bg-gradient-to-r from-purple-400 to-orange-300 text-white transition"
-          : "dark:bg-black"
+          ? "bg-gradient-to-r from-purple-400 to-orange-300 text-white transition border-none"
+          : "dark:bg-black dark:border-2 dark:border-[#0f0f0f]"
       } w-[190px] h-[55px] flex items-center flex-shrink-0`}
       whileTap={{ scale: 1.2 }}
       transition={{ type: "spring", stiffness: 100, damping: 10 }}
