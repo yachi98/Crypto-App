bed402d5098cb34e03575b959ff24c0112402935
Daniel Yachnikov Hughes <daniel.lloyd.hughes@gmail.com>
2024-05-15 00:05:20 +0100

    fixed bug with deployment

--- components/AnimationLoader/index.tsx
+++ components/AnimationLoader/index.tsx
@@ -22,7 +22,6 @@ const AnimationLoader = () => {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
-      xmlns:xlink="http://www.w3.org/1999/xlink"
       version="1.1"
       ref={svgRef}
       width="100"
